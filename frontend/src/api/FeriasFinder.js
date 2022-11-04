4cdb1277aefae2ab41e268e7aa357720152968e0
cabrit0 <cabrit0o.dev@gmail.com>
2022-11-04 19:38:07 +0000

    build2

--- frontend/src/api/FeriasFinder.js
+++ frontend/src/api/FeriasFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1/ferias',
+  baseURL: 'https://feriasapp-api.onrender.com/api/v1/ferias',
 });
--- frontend/src/api/UserFinder.js
+++ frontend/src/api/UserFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1/users',
+  baseURL: 'https://feriasapp-api.onrender.com/api/v1/users',
 });
